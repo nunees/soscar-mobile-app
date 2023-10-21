8a3bb35c4c745f0cf092b93533146f0e2d3609d9
nunees <felipe_silva_nunes@hotmail.com>
2023-10-21 00:50:52 -0300

    feat: create hooks to separate code

---
+++ src/hooks/useUploadImage.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable consistent-return */
+import { AppError } from '@utils/AppError';
+import { IFileInfo } from 'expo-file-system';
+import * as FileSystem from 'expo-file-system';
+import * as ImagePicker from 'expo-image-picker';
+import { useToast } from 'native-base';
+
+export function useUploadImage() {
+  const toast = useToast();
+
+  async function handleUserProfilePictureSelect(id: string): Promise<
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    { userPhotoUploadForm: FormData; photoFile: any } | undefined
+  > {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = (await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        )) as IFileInfo;
+
+        if (photoInfo?.size && photoInfo.size / 1021 / 1024 > 5) {
+          toast.show({
+            title: 'A imagem deve ter no máximo 5MB',
+            placement: 'top',
+            bgColor: 'red.500',
+          });
+        }
+
+        const fileExtension = photoSelected.assets[0].uri.split('.').pop();
+
+        const photoFile = {
+          name: `${id}.${fileExtension}`.toLowerCase(),
+          uri: photoSelected.assets[0].uri,
+          type: `${photoSelected.assets[0].type}/${fileExtension}`,
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } as any;
+
+        const userPhotoUploadForm = new FormData();
+        userPhotoUploadForm.append('avatar', photoFile);
+
+        return { userPhotoUploadForm, photoFile };
+      }
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Erro na atualização';
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+  }
+
+  return { handleUserProfilePictureSelect };
+}
